ff430e4ea1e7da9a551153612d4007847c08c8f5
grubhart <grubhart@gmail.com>
2018-05-11 04:06:20 -0500

    test  3 nodes 1 road

--- src/test/java/org/grubhart/navigator/dijkstra/testDijkstra.java
+++ src/test/java/org/grubhart/navigator/dijkstra/testDijkstra.java
@@ -32,7 +32,7 @@ public class testDijkstra {
     }
 
     @Test
-    public void testProcess_2_nodes(){
+    public void testProcess_2_nodes_1_road(){
 
         Node nodo1=new Node("A");
         AdjacentNode adjacentNode1 = new AdjacentNode("B",1);
@@ -51,6 +51,34 @@ public class testDijkstra {
         assertEquals((Integer) 1,dijkstra.getDistance());
 
     }
+
+    @Test
+    public void testProcess_3_nodes_1_road(){
+
+        Node nodo1=new Node("A");
+        AdjacentNode adjacentNode1 = new AdjacentNode("B",1);
+        nodo1.addAdjacentNode(adjacentNode1);
+
+        Node node2=new Node("B");
+        AdjacentNode adjacentNode2 = new AdjacentNode("C",3);
+        node2.addAdjacentNode(adjacentNode2);
+
+        Node node3=new Node("C");
+
+        List<Node> nodes = new ArrayList<>();
+        nodes.add(nodo1);
+        nodes.add(node2);
+        nodes.add(node3);
+
+        Dijkstra dijkstra=new Dijkstra();
+        dijkstra.init(nodes);
+        dijkstra.process();
+
+        assertEquals((Integer) 4,dijkstra.getDistance());
+
+    }
+
+
 
 
 
